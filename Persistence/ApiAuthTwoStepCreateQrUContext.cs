c1ab7b2974a0923d15ec80643c341a9980dedde1
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-10-03 02:30:56 -0500

    feat: :sparkles: context

--- Persistence/ApiAuthTwoStepCreateQrUContext.cs
+++ Persistence/ApiAuthTwoStepCreateQrUContext.cs
@@ -1,13 +1,21 @@
+using System.Reflection;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence{
-    public class ApiAuthTwoStepCreateQrUContext : DbContext
+namespace Persistence;
+public class ApiAuthTwoStepCreateQrUContext : DbContext
+{
+    public ApiAuthTwoStepCreateQrUContext(DbContextOptions<ApiAuthTwoStepCreateQrUContext> options) : base(options)
     {
-        public ApiAuthTwoStepCreateQrUContext(DbContextOptions<ApiAuthTwoStepCreateQrUContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<User> Users { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
