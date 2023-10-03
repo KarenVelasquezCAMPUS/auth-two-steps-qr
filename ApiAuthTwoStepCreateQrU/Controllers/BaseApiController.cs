31d119b989ec630c225829942b8d36c341b7a422
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-10-03 01:45:45 -0500

    feat: :sparkles: baseApiController

---
+++ ApiAuthTwoStepCreateQrU/Controllers/BaseApiController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuthTwoStepCreateQrU.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+}
