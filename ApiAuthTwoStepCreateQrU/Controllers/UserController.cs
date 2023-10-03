b84c94e7fc0170b4bb36db56267491d163b2c4a4
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-10-03 03:00:41 -0500

    feat: :bug: controller user

--- ApiAuthTwoStepCreateQrU/Controllers/UserController.cs
+++ ApiAuthTwoStepCreateQrU/Controllers/UserController.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ApiAuthTwoStepCreateQrU.Dtos;
 using Domain.Entities;
 using Domain.Interfaces;
 using Microsoft.AspNetCore.Authentication;
@@ -13,12 +10,12 @@ public class UserController : BaseApiController
 {
     private readonly ILogger<UserController> _logger;
     private readonly IUnitOfWork _unitOfWork;
-    private readonly IAuthenticationService _authenticationService;
+    private readonly Domain.Interfaces.IAuthenticationService _authenticationService;
     public UserController
     (
         ILogger<UserController> logger,
         IUnitOfWork unitOfWork,
-        IAuthenticationService authenticationService
+        Domain.Interfaces.IAuthenticationService authenticationService
     )
     {
         _logger = logger;
@@ -40,34 +37,37 @@ public class UserController : BaseApiController
             return File(QR,"image/png");
         }
         catch (Exception ex){
-            _Logger.LogError(ex.Message);
+            _logger.LogError(ex.Message);
             return BadRequest("some wrong");
         }                        
     }
 
-    /* [HttpGet("Verify")]
+    [HttpGet("Verify")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]    
     [ProducesResponseType(StatusCodes.Status400BadRequest)]    
-    public async Task<ActionResult> Verify([FromBody] AuthVerifyCodeDto data)
+    public async Task<ActionResult> Verify([FromBody] AuthenticationVerifyCodeDto data)
     {        
         try
         {
-            User u = await _UnitOfWork.Users.FindFirst(x => x.Id == data.Id);
-            if(u.TwoFactorSecret == null){
-                throw new ArgumentNullException(u.TwoFactorSecret);
+            User u = await _unitOfWork.Users.FindFirst(p => p.UserId == data.Id);
+            if(u.TwoStepSecret == null)
+            {
+                throw new ArgumentNullException(u.TwoStepSecret);
             }
-            var isVerified = _Auth.VerifyCode(u.TwoFactorSecret, data.Code);            
+            var isVerified = _authenticationService.VerifyCode(u.TwoStepSecret, data.Code);            
 
-            if(isVerified == true){
+            if(isVerified == true)
+            {
                 return Ok("authenticated!!");
             }
 
             return Unauthorized();
         }
-        catch (Exception ex){
-            _Logger.LogError(ex.Message);
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
             return BadRequest("some wrong");
         }                        
-    } */
+    }
 }
